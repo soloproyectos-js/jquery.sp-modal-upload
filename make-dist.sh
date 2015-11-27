9b1f9ae454c23d81ad757104b671f53ef0ea18a5
Gonzalo Chumillas <gchumillas@email.com>
2015-11-27 18:38:06 +0100

    update dist

--- make-dist.sh
+++ make-dist.sh
@@ -12,5 +12,5 @@ cat \
 | uglifyjs \
     --compress \
     --mangle \
-    --preamble "/*! jQuery.spModalUpload v0.1.0 | Copyright (c) 2015 Gonzalo Chumillas | https://github.com/soloproyectos-js/jquery.sp-modal-upload/blob/master/LICENSE */" \
+    --preamble "/*! jQuery.spModalUpload v0.2.0 | Copyright (c) 2015 Gonzalo Chumillas | https://github.com/soloproyectos-js/jquery.sp-modal-upload/blob/master/LICENSE */" \
     -o dist/jquery.sp-modal-upload.min.js
